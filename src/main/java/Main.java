b69e859225f0cacb4b6b6c9de5d776ad12bb80bb
Turii <o.turij@gmail.com>
2022-06-09 16:59:35 +0300

    2 commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("hhh");
+        int eee = 20;
+        int i1 = 566;
+        int i = 6;
+        //System.out.println("r" + r);
+        int i2 = 99;
+        System.out.println("hi");
+        int s = 7;
+        int j = System.in.read();
+        System.out.println((char)j);
+    }
+}
---
+++ src/test/java/Class11.java
@@ -0,0 +1,3 @@
+public class Class11 {
+    int g = 88;
+}
